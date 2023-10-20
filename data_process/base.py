5efcd4b0282e643ee11e791108195c502e258092
Thiago Costa Porto <thiagocostaporto@gmail.com>
2023-10-20 17:54:50 +0200

    lint: fix data_process/base.py

--- data_process/base.py
+++ data_process/base.py
@@ -1,15 +1,12 @@
-from tqdm import tqdm
-import pandas as pd
-
 def players_loader(row):
     players = []
-    for team in ['home', 'away']:
-        team_players = row[team + 'PlayersSmoothed']
+    for team in ["home", "away"]:
+        team_players = row[team + "PlayersSmoothed"]
         if team_players is None:
             return None
         else:
             for p in team_players:
-                p['frame_num'] = row['frame_num']
-                p['team'] = team
+                p["frame_num"] = row["frame_num"]
+                p["team"] = team
             players += team_players
     return players
