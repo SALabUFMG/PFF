e6eebca1e6a5b771d21062961b1dba91ad6f3e9f
Thiago Costa Porto <thiagocostaporto@gmail.com>
2023-10-22 02:09:47 +0200

    refactor: consolidate pitch_control/setup.py into a single call

--- pitch_control/setup.py
+++ pitch_control/setup.py
@@ -3,26 +3,21 @@ from Cython.Build import cythonize
 from setuptools import Extension, setup
 
 cyplayer_module = Extension(
-    name="cyplayer", sources=["cyplayer.pyx"], include_dirs=[np.get_include()]
+    name="cyplayer",
+    sources=["cyplayer.pyx"],
+    include_dirs=[np.get_include()],
 )
-
-setup(ext_modules=cythonize([cyplayer_module]))
 
 pc_module = Extension(
-    "cpitchcontrol", sources=["cpitchcontrol.pyx"], include_dirs=[np.get_include()]
+    name="cpitchcontrol",
+    sources=["cpitchcontrol.pyx"],
+    include_dirs=[np.get_include()],
 )
-setup(ext_modules=cythonize([pc_module]))
 
 tp_module = Extension(
-    "ctransitionprobability",
+    name="ctransitionprobability",
     sources=["ctransitionprobability.pyx"],
     include_dirs=[np.get_include()],
 )
-setup(ext_modules=cythonize([tp_module]))
 
-"""setup(
-    name='pc',
-    version='1.0',
-    author='Hugo',
-    ext_modules=[pc_module]
-)"""
+setup(ext_modules=cythonize([cyplayer_module, pc_module, tp_module]))
